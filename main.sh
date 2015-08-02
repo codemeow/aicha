75283c8fb9928dd547242d05746caa3c3a8a9679
codemeow <alexcones@gmail.com>
2015-08-02 19:10:13 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,39 +1,62 @@
 #!/bin/bash
 
-# USE: main.sh MUL.aic INTS ./output.out DEADBEEF
-#        $0      $1      $2      $3        $4
-#     filename command mode    output   initialiser
-#      [M]       [M]    [M]     [O]        [O]
+# «Aicha Intepreter» - Shell interpreter for Aicha programming language
+#
+#  Copyright (C) Alex Cones 2013-2015
+#
+#  This file is part of Project Smilo.
+#
+#  Project Smilo is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Lesser General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  Project Smilo is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#  GNU Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public License
+#  along with Project Smilo. If not, see <http://www.gnu.org/licenses/>.
+
+# USE: main.sh MUL.aic INTS ./output.out DEADBEEF     DEBUG        DEEPNESS
+#        $0      $1      $2      $3        $4          $5            $6
+#     filename command mode    output   initialiser debug messages deepness
+#      [M]       [M]    [M]     [O]        [O]         [O]         [HIDDEN]
 
 PAR0="$0"
 PAR1="$1"
 PAR2="$2"
 PAR3="$3"
 PAR4="$4"
+PAR5="$5"
+PAR6="$6"
 
 if [ -z "$PAR1" ] || [ -z "$PAR2" ]
 then
     echo "Aicha v.0.2 LGPL v3, by ATiger"
-    echo "Use: main.sh <file> <format>"
+    echo "Use: main.sh <file> <format> <output file (opt)> <initialiser (opt)> <debug mode(opt)>"
     echo "Allowed formats: ASCII, HEX, INTS"
+    echo "Allowed initialiser: 00000000..FFFFFFFF"
+    echo "Allowed debug modes: DEBUG or RELEASE"
     exit 0
 fi
 
-if [ "$PAR2" = "HEX" ]
+if [ "$PAR2" == "HEX" ]
 then
    FORMAT="HEX"
-   PAR2="0"
-elif [ "$PAR2" = "ASCII" ]
+elif [ "$PAR2" == "ASCII" ]
 then
    FORMAT="ASCII"
-   PAR2="0"
-elif [ "$PAR2" = "INTS" ]
+elif [ "$PAR2" == "INTS" ]
 then
    FORMAT="INTS"
-   PAR2="0"
+else
+   echo "ERR: Unknown output format"
+   exit 2
 fi
 
-if [ -z "$PAR3" ]
+if [ -z "$PAR3" ] && [ "$DEEPNESS" = 0 ]
 then
     OUTPUT="./output.txt"
 else
@@ -47,12 +70,32 @@ else
     INITER="$PAR4"
 fi
 
-# ## Registers table
-# # REGIST00 - REGIST7F - For user use
-# # REGIST80 - REGISTFC - For system use
-# # REGISTFD            - lvalue parameter for custom functions also output register
-# # REGISTFE            - rvalue parameter for custom functions
-# # REGISTFF            - Caret pos
+if [ -z "$PAR5" ]
+then
+    DEBUG_MODE="RELEASE"
+else
+    if [[ "DEBUG RELEASE" =~ "$PAR5" ]]
+    then
+        DEBUG_MODE="$PAR5"
+    else
+        echo "ERR: Bad debug mode"
+        exit 2
+    fi
+fi
+
+if [ -z "$PAR6" ]
+then
+    DEEPNESS=0
+else
+    DEEPNESS="$PAR6"
+fi
+
+## Registers table
+# REGIST00 - REGIST7F - For user use
+# REGIST80 - REGISTFC - For system use
+# REGISTFD            - lvalue parameter for custom functions also output register
+# REGISTFE            - rvalue parameter for custom functions
+# REGISTFF            - Caret pos
 
 REGISTLEFT="REGISTFD"
 REGISTRGHT="REGISTFE"
@@ -60,7 +103,7 @@ REGISTRGHT="REGISTFE"
 mkdir sys 2>/dev/null
 mkdir usr 2>/dev/null
 
-if [ "$PAR2" = 0 ]
+if [ "$DEEPNESS" == 0 ]
 then
     echo "# Creating registers"
     mkdir registers 2>/dev/null
@@ -72,8 +115,8 @@ then
     done
 fi
       
-for ((i=0; i < "$PAR2"; i++)) do echo -n "│"; done; echo "┌───"
-for ((i=0; i < "$PAR2"; i++)) do echo -n "│"; done; echo -n "│"
+for ((i=0; i < "$DEEPNESS"; i++)) do echo -n "│"; done; echo "┌───"
+for ((i=0; i < "$DEEPNESS"; i++)) do echo -n "│"; done; echo -n "│"
 basename "$PAR1" .aic
 
 OLDIFS="$IFS"
@@ -82,7 +125,7 @@ IFS="$OLDIFS"
 
 echo "00000000" > ./registers/REGISTFF
 
-while (( ${#LINES[@]} >  0x$(cat ./registers/REGISTFF)))
+while (( ${#LINES[@]} > 0x$(cat ./registers/REGISTFF)))
 do
     line="${LINES[0x$(cat ./registers/REGISTFF)]}"
 
@@ -177,26 +220,31 @@ do
             if [ -n "$(find ./sys/ -name $command.aic | tr -d '\n')" ]
             then                
                 OLDCARET=$((0x$(cat ./registers/REGISTFF)))
-                sh ./main.sh "./sys/$command.aic" $(($2 + 1))
+                ./main.sh "./sys/$command.aic" $FORMAT "NO" "NO" $DEBUG_MODE $(($DEEPNESS + 1))
                 printf %08X "$OLDCARET" > "./registers/REGISTFF"
             elif [ -n "$(find ./usr/ -name $command.aic | tr -d '\n')" ]
             then
                 OLDCARET=$((0x$(cat ./registers/REGISTFF)))
-                sh ./main.sh "./usr/$command.aic" $(($2 + 1))
+                ./main.sh "./usr/$command.aic" $FORMAT "NO" "NO" $DEBUG_MODE $(($DEEPNESS + 1))
                 printf %08X "$OLDCARET" > "./registers/REGISTFF"
             else
                 echo "ERR: Command \"$command\" not found"
                 exit 2;
             fi;;
+        "#" ) 
+        if [ "$DEBUG_MODE" == "DEBUG" ]
+        then
+            for ((i=0; i < "$DEEPNESS"; i++)) do echo -n "│"; done; echo "│ $line"
+        fi;;
     esac
     
     TT=$((0x$(cat ./registers/REGISTFF) + 1))
     printf %08X "$TT" > "./registers/REGISTFF"
 done
 
-for ((i=0; i < "$PAR2"; i++)) do echo -n "│"; done; echo "└───"
+for ((i=0; i < "$DEEPNESS"; i++)) do echo -n "│"; done; echo "└───"
 
-if [ "$PAR2" -eq 0 ]
+if [ "$DEEPNESS" == 0 ]
 then
     echo "# Creating output"
     rm -f "$OUTPUT"
