35e5a47ff10aca735346b8e1df06548ae3664016
codemeow <alexcones@gmail.com>
2015-08-05 23:09:58 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,23 +1,19 @@
 #!/bin/bash
 
 # «Aicha Intepreter» - Shell interpreter for Aicha programming language
-#
-#  Copyright (C) Alex Cones 2013-2015
-#
-#  This file is part of Project Smilo.
-#
-#  Project Smilo is free software: you can redistribute it and/or modify
-#  it under the terms of the GNU Lesser General Public License as published by
-#  the Free Software Foundation, either version 3 of the License, or
-#  (at your option) any later version.
-#
-#  Project Smilo is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-#  GNU Lesser General Public License for more details.
-#
-#  You should have received a copy of the GNU Lesser General Public License
-#  along with Project Smilo. If not, see <http://www.gnu.org/licenses/>.
+##
+##  Project Aicha is free software: you can redistribute it and/or modify
+##  it under the terms of the GNU Lesser General Public License as published by
+##  the Free Software Foundation, either version 3 of the License, or
+##  (at your option) any later version.
+##
+##  Project Aicha is distributed in the hope that it will be useful,
+##  but WITHOUT ANY WARRANTY; without even the implied warranty of
+##  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+##  GNU Lesser General Public License for more details.
+##
+##  You should have received a copy of the GNU Lesser General Public License
+##  along with Project Aicha. If not, see <http://www.gnu.org/licenses/>.
 
 # USE: main.sh MUL.aic INTS ./output.out DEADBEEF     DEBUG        DEEPNESS
 #        $0      $1      $2      $3        $4          $5            $6
